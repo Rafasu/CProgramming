fb70dcf6240abd3ebd4e75f60f849f118777c363
rafasu <rafasu8@gmail.com>
2017-03-14 18:18:14 -0600

    Modified name of the file

---
+++ majorityElement.cpp
@@ -0,0 +1,25 @@
+//169. LeetCode
+
+//Moores Voting Algorithm.
+//Notes: Assumptions: No empty vector. There's a majority element.
+
+class solution{
+public: 
+	int majorityElement(vector <int> &nums){
+		int iMajor, iCont = 0; 
+		int i, n =  nums.size() ;
+
+		for( i  = 0; i < n ; i++){
+			if(iCont == 0){
+				iMajor = nums[i] ;
+				iCont++ ;
+			}
+			else if(iMajor == nums[i] ;
+				iCont++ ;
+			else
+				iCont-- ;
+		}
+		return iMajor ;
+	}
+
+};
